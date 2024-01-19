d010b943c5c35e44538464665e48aacd7b2e7096
Zachary Weston <zachary.weston@propellerhead.co.nz>
2024-01-20 09:21:51 +1300

    cleaning transitions

--- src/views/Content/blocks/HeroBanner.tsx
+++ src/views/Content/blocks/HeroBanner.tsx
@@ -1,6 +1,7 @@
-import { JSX, useState, useEffect, useLayoutEffect } from "react";
+import { JSX, useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
+import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 
 import { Functions } from "@/functions";
@@ -20,9 +21,7 @@ const HeroBanner = (props: HerobannerTypes): JSX.Element => {
 
     useEffect(() => {
         functions.imageSrc({ setLoad, src });
-    }, [src]);
 
-    useLayoutEffect(() => {
         const handleScroll = () => {
             setScrollPosition(window.scrollY);
         };
@@ -31,7 +30,7 @@ const HeroBanner = (props: HerobannerTypes): JSX.Element => {
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    });
+    }, [src]);
 
     const heroImage = {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${src})`,
@@ -42,43 +41,47 @@ const HeroBanner = (props: HerobannerTypes): JSX.Element => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         transform: `translateY(${scrollPosition * parallaxDepth}px) scale(1.1)`,
-        transition: "transform 0.1s ease-out",
-        webkitTransform: `translateY(${
-            scrollPosition * parallaxDepth
-        }px) scale(1.1)`,
         zIndex: -1,
-    };
-
-    const skeletonImg = {
-        height,
-        backgroundColor: "primary.main",
     };
 
     const textBlock = {
         zIndex: 1,
-        pl: { lg: 12, md: 6, sm: 4, xs: 2 },
+        pl: { lg: 20, md: 6, sm: 4, xs: 2 },
         pt: 40,
         position: "relative",
-        transform: `translateY(${scrollPosition * -0.25}px)`,
-        transition: "transform 0.1s ease-out",
+        transform: `translateY(${
+            scrollPosition * -parallaxDepth
+        }px) scale(1.1)`,
     };
 
     return (
         <Box sx={{ overflow: "hidden" }} id="hero-banner">
-            <Box id="background-image" sx={load ? skeletonImg : heroImage}>
-                <Box id="text-block" sx={textBlock}>
-                    <Typography id="hero-headline" variant="h1" color="white">
-                        {contentEntry.fields.headline}
-                    </Typography>
-                    <Typography
-                        id="hero-body"
-                        variant="h5"
-                        color="white"
-                        sx={{ maxWidth: "50%", my: 2 }}>
-                        {contentEntry.fields.body}
-                    </Typography>
+            {!load ? (
+                <Box id="background-image" sx={heroImage}>
+                    <Box id="text-block" sx={textBlock}>
+                        <Typography
+                            id="hero-headline"
+                            variant="h1"
+                            color="white">
+                            {contentEntry.fields.headline}
+                        </Typography>
+                        <Typography
+                            id="hero-body"
+                            variant="h5"
+                            color="white"
+                            sx={{ maxWidth: "50%", my: 2 }}>
+                            {contentEntry.fields.body}
+                        </Typography>
+                    </Box>
                 </Box>
-            </Box>
+            ) : (
+                <Skeleton
+                    id="img-skeleton"
+                    variant="rectangular"
+                    width="100%"
+                    height={height.xl}
+                />
+            )}
         </Box>
     );
 };
