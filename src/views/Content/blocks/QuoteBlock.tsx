4f9cdc774d86de9fbc867f008ae5e52d33c9ff35
Zachary Weston <zachary.weston@propellerhead.co.nz>
2024-02-15 09:44:02 +1300

    tidied blockQuote

--- src/views/Content/blocks/QuoteBlock.tsx
+++ src/views/Content/blocks/QuoteBlock.tsx
@@ -1,4 +1,3 @@
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
@@ -9,18 +8,16 @@ export const QuoteBlock = (props: GenericContent): JSX.Element => {
 
     return (
         <Grid
-            sx={{ height: { lg: "35vw", xs: 400 } }}
+            sx={{ height: { lg: "30vw", xs: 400 } }}
             container
             direction="column"
             justifyContent="center"
             alignItems="center"
             spacing={2}>
             <Grid item>
-                <Container maxWidth={false} sx={{ maxWidth: 1000 }}>
-                    <Typography variant="h3" align="center">
-                        {contentEntry.fields.body}
-                    </Typography>
-                </Container>
+                <Typography variant="h3" align="center" sx={{ maxWidth: 950 }}>
+                    {contentEntry.fields.body}
+                </Typography>
             </Grid>
         </Grid>
     );
